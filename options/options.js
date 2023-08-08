8cfdac070ca3dd175f981502c815bd980d18d55e
Crowdbotics <engineering@crowdbotics.com>
2023-08-08 02:25:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-resonance-d-107953",
+  url: "https://gentle-resonance-d-107953.botics.co",
+  api: "https://gentle-resonance-d-107953.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
